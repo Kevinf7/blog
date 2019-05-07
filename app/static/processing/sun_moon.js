5838519e3830ece5c1fa1d0507b50163b0181c11
Kevin Foong <kevin_foong@yahoo.com>
2019-05-07 22:11:55 +1000

    minor

--- app/static/processing/sun_moon.js
+++ app/static/processing/sun_moon.js
@@ -21,7 +21,7 @@ function draw() {
   }
 }
 
-function mousePressed() {
+function mouseClicked() {
   moons.push(new Moon(mouseX,mouseY));
 }
 
@@ -32,34 +32,34 @@ class Sun {
     //size of sun
     this.rad=random(8,50);
   }
-  
+
   display() {
     fill(255,0,0,200);
     ellipse(this.x,this.y,this.rad*2,this.rad*2);
   }
-  
+
   getx () {
     return this.x;
   }
-  
+
   gety() {
     return this.y;
   }
-  
+
   getrad() {
     return this.rad;
   }
 }
 
 class Moon {
-  
+
   constructor(x,y) {
     this.x=x;
     this.y=y;
     this.rad=4;
     this.loccount=0; //for the lerp
     this.startspin=false;
-    
+
     // calculate distance to each sun
     this.adj = [];
     this.opp = [];
@@ -69,7 +69,7 @@ class Moon {
       this.opp[i] = suns[i].gety()-this.y;
       this.dist[i] = sqrt(pow(this.adj[i],2)+pow(this.opp[i],2));
     }
-    
+
     // for the closest sun, get the index and the distance
     this.closestsun_index = 0;
     this.closestsun_dist = 99999999;
@@ -79,7 +79,7 @@ class Moon {
         this.closestsun_dist = this.dist[i];
       }
     }
-    
+
     // get theta
     this.theta = atan2(this.opp[this.closestsun_index],this.adj[this.closestsun_index])+PI/2;
     // get the sun object
@@ -91,12 +91,12 @@ class Moon {
     // calculate spin speed
     this.spinspeed = random(-0.04,0.04);
   }
- 
+
   display() {
     fill(0,80,80,200);
     if (!this.startspin) {
-      this.locx = lerp(this.x, this.sun.getx(), this.loccount/this.closestsun_dist); 
-      this.locy = lerp(this.y, this.sun.gety(), this.loccount/this.closestsun_dist); 
+      this.locx = lerp(this.x, this.sun.getx(), this.loccount/this.closestsun_dist);
+      this.locy = lerp(this.y, this.sun.gety(), this.loccount/this.closestsun_dist);
       ellipse(this.locx,this.locy,this.rad*2,this.rad*2);
       this.loccount+=1.6;
     } else {
@@ -104,7 +104,7 @@ class Moon {
       this.locy = this.sun.gety() + cos(this.theta)*this.radmotion;
       ellipse(this.locx,this.locy,this.rad*2,this.rad*2);
       this.theta += this.spinspeed;
-      
+
     }
     if(this.loccount>=this.distspin){
       this.startspin=true;
