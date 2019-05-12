407943b0574a958465864a2b3811362991dfcf3f
Kevin Foong <kevin_foong@yahoo.com>
2019-05-12 21:17:40 +1000

    tooltip

--- sql.sql
+++ sql.sql
@@ -6,8 +6,7 @@ UPDATE role
 SET `default` = 1
 WHERE name = 'user';
 
-INSERT INTO page (name,content,page_id,update_date)
-VALUES ('content1','test',7,now()),('content1','test',8,now()),('content1','test',9,now());
+INSERT INTO content (name,content,page_id,update_date)
+VALUES ('content1','test',1,now()),('content1','test',2,now());
 
-INSERT INTO content (name,update_date)
-VALUES ('main',now()),('about',now()),('contact',now());
+INSERT INTO page (name) VALUES ('about'),('contact');
