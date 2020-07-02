b2d5eadf10b8d8d48d62e60dbcf19b45269872d9
Kevin Foong <kevin_foong@yahoo.com>
2020-07-02 22:04:59 +1000

    add ads.txt

--- config.py
+++ config.py
@@ -5,14 +5,16 @@ from pathlib import Path
 basedir = Path(__file__).parent
 load_dotenv(basedir / '.env')
 
+
 class Config(object):
     # Flask settings
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'random string'
 
     # Flask-SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # recommended by Pythonanywhere otherwise you get mysql timeout
     SQLALCHEMY_POOL_RECYCLE = 299
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # number of blog posts to show per page
     POSTS_PER_PAGE = 6
     # auto reload template without needing to restart Flask
@@ -28,10 +30,10 @@ class Config(object):
     MAIL_ADMINS = os.environ.get('MAIL_ADMINS').split(' ')
 
     # Google recaptcha
-    RECAPTCHA_USE_SSL= False
-    RECAPTCHA_PUBLIC_KEY=os.environ.get('RECAPTCHA_PUBLIC_KEY')
-    RECAPTCHA_PRIVATE_KEY=os.environ.get('RECAPTCHA_PRIVATE_KEY')
-    RECAPTCHA_OPTIONS= {'theme':'black'}
+    RECAPTCHA_USE_SSL = False
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
+    RECAPTCHA_OPTIONS = {'theme': 'black'}
 
     # Processing folder
     PROCESSING_FOLDER = basedir / 'app/static/processing'
@@ -40,7 +42,7 @@ class Config(object):
     RENTAL_FOLDER = basedir / 'app/static/rental'
 
     # custom app settings
-    FORGOT_PASSWORD_TOKEN_EXPIRE = 3600 # in seconds, 3600 = 1 hour
+    FORGOT_PASSWORD_TOKEN_EXPIRE = 3600  # in seconds, 3600 = 1 hour
     SEARCH_RESULTS_RETURN = 12
     # admin number of messages
     MESSAGES_PER_PAGE = 10
